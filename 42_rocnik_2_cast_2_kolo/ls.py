4df790bc535b5adbaabb5554afce2e5edd5d7418
MilosCicmanec <171834077+MilosCicmanec@users.noreply.github.com>
2025-05-29 15:34:51 +0200

    The third problem not the final solution

---
+++ 42_rocnik_2_cast_2_kolo/ls.py
@@ -0,0 +1,23 @@
+string = list(input())
+
+
+def delete_string(s):
+    moves = 0
+    s = list(s)
+
+    while s:
+        found = False
+       
+        for end in range(len(s) - 1, 0, -1):
+            if s[0] == s[end]:
+               
+                del s[0:end+1]
+                moves += 1
+                found = True
+                break
+        if not found:
+            
+            del s[0]
+            moves += 1
+
+    return moves
